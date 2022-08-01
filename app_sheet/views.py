dcff93c060cb6e7eba239aefeb9452884c3034f0
graham <graham@willsfamily.org>
2022-08-01 17:25:26 -0500

    more complex forms

--- app_sheet/views.py
+++ app_sheet/views.py
@@ -1,22 +1,64 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 
-def index(request):
-    print('Request for index page received')
-    return render(request, 'app_sheet/index.html')
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
 
-@csrf_exempt
-def hello(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        
-        if name is None or name == '':
-            print("Request for hello page received with no name or blank name -- redirecting")
-            return redirect('index')
-        else:
-            print("Request for hello page received with name=%s" % name)
-            context = {'name': name }
-            return render(request, 'app_sheet/hello.html', context)
-    else:
-        return redirect('index')
+from app_sheet.models import CharacterSheetDefinition
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app_sheet/index.html',
+        {
+            'title': 'Home Page',
+            'year': datetime.now().year,
+        }
+    )
+
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app_sheet/contact.html',
+        {
+            'title': 'Contact',
+            'message': 'ZeeSheet Contact Page',
+            'year': datetime.now().year,
+        }
+    )
+
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app_sheet/about.html',
+        {
+            'title': 'About',
+            'message': 'About ZeeSheet',
+            'year': datetime.now().year,
+        }
+    )
+
+
+def sheet_edit(request, sheet_id):
+    csd = get_object_or_404(CharacterSheetDefinition, pk=sheet_id)
+    return render(
+        request,
+        'app_sheet/sheetedit.html',
+        {
+            'title': 'Sheet',
+            'sheet': csd,
+            'year': datetime.now().year,
+        }
+    )
+
+
+def markdown_view(request):
+    return HttpResponse("This is where the markdown component will go")
