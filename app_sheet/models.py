cfd43fa977448b812d21a4d5c9e83e9a49b64a5d
graham <graham@willsfamily.org>
2022-08-02 08:49:04 -0500

    synch databases

--- app_sheet/models.py
+++ app_sheet/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class CharacterSheetDefinition(models.Model):
     owner = models.CharField(name='Owner ID', max_length=256, blank=True)
-    content = models.TextField(name='Content', verbose_name='Input Content as Markdown')
+    content = models.TextField(name='Content', verbose_name='Definition in restructured text')
     created = models.DateField(name='Date Created', auto_now_add=True)
     modified = models.DateField(name='Date Last Modified', auto_now=True)
