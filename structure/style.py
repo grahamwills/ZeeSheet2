77e9f18bcdd6fb9dfe0fdd8c43ea29859edc9af6
graham <graham@willsfamily.org>
2022-09-06 10:16:07 -0500

    Styles as strings

--- structure/style.py
+++ structure/style.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from typing import List
 
 from reportlab.lib import units
-from reportlab.lib.colors import Color, toColor
+from reportlab.lib.colors import toColor
 
 from common import Spacing
 
@@ -28,90 +29,162 @@ class FontStyle:
     size: float = None
     style: str = None
 
-    def set(self, name: str, value):
-        key = name.lower().replace('-', '_')
+    def set(self, key: str, value):
         if key in ['font', 'fontfamily', 'family']:
             self.family = value
         elif key == 'size':
             self.size = float(value)
         elif key in ['style', 'face']:
             if value.lower() in {'normal', 'regular', 'bold', 'italic', 'bolditalic'}:
-                self.align = value.lower()
+                self.style = value.lower()
             else:
                 raise ValueError(
-                    f"Unknown value {value} for alignment -- should be one of: regular, bold, italic, boldItalic")
+                    f"Unknown value {value} for alignment -- should be one of:regular, bold, italic, boldItalic")
         else:
             raise AttributeError(key)
+
+    def add_to_definition(self, parts: List[str]) -> None:
+        if self.family:
+            parts.append(f'font-family:{self.family}')
+        if self.size is not None:
+            parts.append(f'font-size:{self.size}')
+        if self.style is not None:
+            parts.append(f'font-style:{self.style}')
 
 
 @dataclass
 class TextStyle:
-    color: Color = None
+    color: str = None
     opacity: float = None
     align: str = None
     indent: float = None
 
-    def set(self, name: str, value):
-        key = name.lower().replace('-', '').replace('_', '')
+    def set(self, key: str, value):
         if key.startswith('text'):
             key = key[4:]
         if key in ['color', 'foreground']:
-            self.color = toColor(value)
+            toColor(value)  # Check it is valid
+            self.color = value
         elif key == 'opacity':
             self.opacity = float(value)
         elif key == 'align':
             if value in {'left', 'right', 'center'}:
                 self.align = value
             else:
-                raise ValueError(f"Unknown value {value} for alignment -- should be one of: left, right, center")
+                raise ValueError(f"Unknown value {value} for alignment -- should be one of:left, right, center")
         elif key in ['indent', 'indentation']:
             self.indent = float(value)
         else:
             raise AttributeError(key)
 
+    def add_to_definition(self, parts: List[str]) -> None:
+        if self.color:
+            parts.append(f'text-color:{self.color}')
+        if self.opacity is not None:
+            parts.append(f'text-opacity:{self.opacity}')
+        if self.align is not None:
+            parts.append(f'text-align:{self.align}')
+        if self.indent is not None:
+            parts.append(f'text-indent:{self.indent}')
+
+
+def _len2str(x: float) -> str:
+    if x == 0:
+        return 0
+    if x % 9 == 0:
+        return f'{x / 9}in'
+    elif x == int(x):
+        return str(int(x))
+    else:
+        return str(x)
+
+
+def spacing_to_text(spacing: Spacing) -> str:
+    parts = (_len2str(spacing.left), _len2str(spacing.right), _len2str(spacing.top), _len2str(spacing.bottom))
+    if parts[0] == parts[1] == parts[2] == parts[3]:
+        return '{}'.format(parts[0])
+    if parts[0] == parts[1] and  parts[2] == parts[3]:
+        return '{} {}'.format(parts[0], parts[1])
+    else:
+        return '{} {} {} {}'.format(*parts)
+
 
 @dataclass
 class BoxStyle:
+    color: str = None
+    opacity: float = None
     border: str = None
     width: float = None
-    color: Color = None
-    opacity: float = None
+    border_color: str = None
+    border_opacity: float = None
     margin: Spacing = None
     padding: Spacing = None
 
-    def set(self, name: str, value):
-        key = name.lower().replace('-', '').replace('_', '')
+    def set(self, key: str, value):
         if key.startswith('box'):
             key = key[3:]
-        if key in {'color', 'backgroundcolor'}:
-            self.color = toColor(value)
-        elif key in {'opacity', 'backgroundopacity'}:
+        if key in {'color', 'backgroundcolor', 'background', 'bg'}:
+            toColor(value)  # Check it is valid
+            self.color = value
+        elif key in {'opacity', 'backgroundopacity', 'bgopacity'}:
             self.opacity = float(value)
+        if key == 'bordercolor':
+            toColor(value)  # Check it is valid
+            self.border_color = value
+        if key == 'borderopacity':
+            toColor(value)  # Check it is valid
+            self.border_opacity = value
         elif key in {'border', 'method', 'style'}:
             if value in {'none', 'square', 'rounded'}:
                 self.border = value
             else:
-                raise ValueError(f"Unknown value {value} for border -- should be one of: none, square, rounded")
+                raise ValueError(f"Unknown value {value} for border -- should be one of:none, square, rounded")
         elif key == 'margin':
             self.margin = text_to_spacing(value)
         elif key == 'padding':
             self.padding = text_to_spacing(value)
-        elif key in ['width', 'size', 'linewidth']:
+        elif key in ['width', 'size', 'linewidth', 'borderwidth']:
             self.width = units.toLength(value)
         else:
             raise AttributeError(key)
+
+    def add_to_definition(self, parts: List[str]) -> None:
+        if self.border:
+            parts.append(f'border:{self.border}')
+        if self.width:
+            parts.append(f'border-width:{self.width}')
+        if self.border_color:
+            parts.append(f'border-color:{self.border_color}')
+        if self.border_opacity is not None:
+            parts.append(f'border-opacity:{self.border_opacity}')
+        if self.color:
+            parts.append(f'background-color:{self.color}')
+        if self.opacity is not None:
+            parts.append(f'background-opacity:{self.opacity}')
+        if self.margin is not None:
+            parts.append(f'margin:{spacing_to_text(self.margin)}')
+        if self.padding is not None:
+            parts.append(f'indent:{spacing_to_text(self.padding)}')
 
 
 @dataclass
 class Style:
     """ A complete style  """
     name: str
-    parent: Style
+    parent: str = None
     text: TextStyle = field(default_factory=TextStyle)
     font: FontStyle = field(default_factory=FontStyle)
     box: BoxStyle = field(default_factory=BoxStyle)
 
+    def __post_init__(self):
+        if not self.name.isidentifier():
+            raise ValueError(f'Style name must be a valid identifier, but was {self.name}')
+
     def set(self, key: str, value: str):
+        key = key.lower().replace('-', '').replace('_', '')
+        if key in {'parent', 'inherit'}:
+            self.parent = value
+
         children = ('text', 'font', 'box')
 
         # Check if the attribute has a child name as prefix
@@ -129,17 +202,29 @@ class Style:
 
         raise AttributeError(key)
 
+    def to_definition(self):
+        parts = []
+        if self.parent:
+            parts.append(f'inherit:{self.parent}')
+        self.text.add_to_definition(parts)
+        self.font.add_to_definition(parts)
+        self.box.add_to_definition(parts)
+        return '; '.join(parts)
 
-class Default:
+
+class Defaults:
     """ Default Values """
 
     # noinspection PyTypeChecker
     base = Style(
         'default',
         None,
-        TextStyle(Color(0, 0, 0), 1.0, 'left', 0),
+        TextStyle('black', 1.0, 'left', 0),
         FontStyle('Helvetica', 12, 'normal'),
-        BoxStyle('none', 1, Color(1, 1, 1), 0.0, Spacing.balanced(0), Spacing.balanced(0)))
+        BoxStyle(
+            'white', 0.0,
+            'none', 1, 'black', 1.0,
+            Spacing.balanced(0), Spacing.balanced(0)))
 
 
 def set_style_value(obj, name: str, value: str):
--- tests/test_style.py
+++ tests/test_style.py
@@ -1,12 +1,35 @@
 from unittest import TestCase
 
-from reportlab.lib.colors import Color
-
-from structure.style import Style, set_style_value, Default
+from common import Spacing
+from structure.style import Style, Defaults
 
 
 class TestStyle(TestCase):
 
     def test_default(self):
-        s = Default.base
-        self.assertEqual(s.text.color, Color(0,0,0))
+        # Tests a few items
+        self.assertEqual(Defaults.base.name, 'default')
+        self.assertEqual(Defaults.base.parent, None)
+        self.assertEqual(Defaults.base.text.align, 'left')
+        self.assertEqual(Defaults.base.font.family, 'Helvetica')
+        self.assertEqual(Defaults.base.box.opacity, 0.0)
+        self.assertEqual(Defaults.base.box.padding, Spacing(0, 0, 0, 0))
+
+    def test_initialization(self):
+        s = Style('allowed_name')
+        self.assertIsNotNone(s.font)
+        self.assertIsNotNone(s.text)
+        self.assertIsNotNone(s.box)
+        self.assertRaises(ValueError, lambda: Style('bad name'))
+        self.assertRaises(ValueError, lambda: Style('1badname'))
+
+    def test_to_definition_default(self):
+        self.assertEqual('text-color:black; text-opacity:1.0; text-align:left; text-indent:0; '
+                         'font-family:Helvetica; font-size:12; font-style:normal; '
+                         'border:none; border-width:1; '
+                         'border-color:black; border-opacity:1.0; '
+                         'background-color:white; background-opacity:0.0; '
+                         'margin:0; indent:0', Defaults.base.to_definition())
+
+    def test_to_definition_empty(self):
+        self.assertEqual('inherit:default', Style('test', 'default').to_definition())
