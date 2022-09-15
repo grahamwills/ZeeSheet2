f66d15f16da6a6036ae696f513f54e139a1416f9
graham <graham@willsfamily.org>
2022-09-15 09:32:12 -0500

    More fonts

--- generate/font_initialization.py
+++ generate/font_initialization.py
@@ -205,18 +205,21 @@ def generate_docs():
 def copy_temp():
     src = Path('/Users/graham/Desktop/gf')
     dest = Path('/Users/graham/Documents/SoftwareProjects/ZeeSheet2/generate/resources/google-fonts')
-    items = {f.stem.split('-')[0] for f in src.glob('*-Bold.ttf')} & \
-            {f.stem.split('-')[0] for f in src.glob('*-Italic.ttf')}
+    items = {f.stem.split('-')[0] for f in src.glob('*-Bold.ttf')} \
+        .union({f.stem.split('-')[0] for f in src.glob('*-Italic.ttf')})
+    n = 0
     for name in items:
         for ext in 'Regular Bold Italic BoldItalic'.split():
             file = src / (name + '-' + ext + '.ttf')
             if not file.exists():
                 print('Nope:', file)
             else:
+                n += 1
                 nfile = dest / file.name
+                print(f"{file.name:40}{nfile}")
                 file.rename(nfile)
 
-
+    print('\n', n, ' files')
 
 if __name__ == '__main__':
     # copy_fonts_locally()
