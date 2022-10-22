47d2cbd90ecccc83998db9069af8519416ca69b3
graham <graham@willsfamily.org>
2022-10-21 23:43:38 -0500

    more simplification

--- tests/test_quality.py
+++ tests/test_quality.py
@@ -1,11 +1,12 @@
 import unittest
+from dataclasses import dataclass
+from typing import Any
 
 from reportlab.lib.colors import Color
 
 import layout.quality as quality
 from common import Extent, Rect, Point
 from drawing import TextSegment, CheckboxSegment
-from layout import PlacedImageContent, PlacedRectContent, PlacedRunContent, PlacedGroupContent
 from structure import ImageDetail
 from structure import Style
 
@@ -13,15 +14,33 @@ POINT = Point(2, 3)
 EXTENT = Extent(1, 1)
 STYLE = Style('foo')
 BOUNDS = Rect(2, 3, 3, 4)
-COLOR = Color(0,0,0)
+COLOR = Color(0, 0, 0)
 
 IMAGE = ImageDetail(2, None, 100, 200)
-SEGMENTS = [TextSegment('hello', 2, 3, 12, None, COLOR), CheckboxSegment(True, 15, 3, 10, None,COLOR)]
+SEGMENTS = [TextSegment('hello', 2, 3, 12, None, COLOR), CheckboxSegment(True, 15, 3, 10, None, COLOR)]
 
 IM_DESIRED = Extent(100, 200)
 
 
+@dataclass
+class A:
+    quality: Any
+
+
 # noinspection PyTypeChecker
+def quality_for_table(table: list[list], extra: float):
+    # Put the qualities from the table into a fake element
+    elements = [[A(i) if i is not None else None for i in items] for items in table]
+    return quality.for_table(elements, extra)
+
+
+# noinspection PyTypeChecker
+def quality_for_columns(widths, table: list[list], extra: float):
+    # Put the qualities from the table into a fake element
+    elements = [[A(i) if i is not None else None for i in items] for items in table]
+    return quality.for_columns(widths, elements, extra)
+
+
 class TestQuality(unittest.TestCase):
 
     def test_str_for_decoration(self):
@@ -40,7 +59,7 @@ class TestQuality(unittest.TestCase):
         a = quality.for_wrapping(1, 4, 1)
         b = quality.for_wrapping(31, 0, 5)
         c = quality.for_decoration()
-        q = quality.for_table([[a, b, c], [a, None, c]], 7)
+        q = quality_for_table([[a, b, c], [a, None, c]], 7)
 
         # Three non-decorative items
         # Total desired width is 700 of which 124+154 is used
@@ -49,14 +68,14 @@ class TestQuality(unittest.TestCase):
         self.assertEqual('⟨TABLE(3), excess=1, unplaced=7, breaks=8•7⟩', str(q))
 
         # With no unplaced
-        q = quality.for_table([[a, b, c], [a, None, c]], 0)
+        q = quality_for_table([[a, b, c], [a, None, c]], 0)
         self.assertEqual('⟨TABLE(3), excess=1, breaks=8•7⟩', str(q))
 
     def test_str_for_columns(self):
         a = quality.for_wrapping(1, 4, 1)
         b = quality.for_wrapping(31, 0, 5)
         c = quality.for_decoration()
-        q = quality.for_columns([20, 90], [[a, b, c, c], [a, None, c]], 7)
+        q = quality_for_columns([20, 90], [[a, b, c, c], [a, None, c]], 7)
 
         # Max height is 90, standard deviation = 35
         self.assertEqual('⟨COLUMNS(3), excess=1, unplaced=7, breaks=8•7, ∆height=35⟩', str(q))
@@ -65,8 +84,8 @@ class TestQuality(unittest.TestCase):
         a = quality.for_wrapping(1, 4, 1)
         b = quality.for_wrapping(1, 3, 4)
         c = quality.for_decoration()
-        d = quality.for_table([[a, b, c], [a, None, b]], 7)
-        e = quality.for_table([[a, b, c], [b, None, None]], 8)
+        d = quality_for_table([[a, b, c], [a, None, b]], 7)
+        e = quality_for_table([[a, b, c], [b, None, None]], 8)
 
         # Does not raise
         a.better(None)
@@ -127,12 +146,12 @@ class TestQualityComparisonForGroups(unittest.TestCase):
         b = quality.for_wrapping(31, 0, 5)
         c = quality.for_decoration()
 
-        cols1 = quality.for_table([[a, b, a], [b, b, b]], 3)
-        cols2 = quality.for_table([[a, c, b, a], [b, b, b, c]], 3)
+        cols1 = quality_for_table([[a, b, a], [b, b, b]], 3)
+        cols2 = quality_for_table([[a, c, b, a], [b, b, b, c]], 3)
         self.assertAlmostEqual(cols1.minor_score(), cols2.minor_score())
 
-        cols1 = quality.for_columns([50, 75], [[a, b, a], [b, b, b]], 3)
-        cols2 = quality.for_columns([50, 75], [[a, c, b, a], [b, b, b, c]], 3)
+        cols1 = quality_for_columns([50, 75], [[a, b, a], [b, b, b]], 3)
+        cols2 = quality_for_columns([50, 75], [[a, c, b, a], [b, b, b, c]], 3)
         self.assertAlmostEqual(cols1.minor_score(), cols2.minor_score())
 
     def test_aggregation(self):
@@ -142,7 +161,7 @@ class TestQualityComparisonForGroups(unittest.TestCase):
         d = quality.for_image('normal', Extent(200, 200), Rect(0, 100, 0, 200), Rect(0, 100, 0, 200))
         e = quality.for_image('normal', Extent(300, 200), Rect(0, 100, 0, 200), Rect(0, 100, 0, 200))
 
-        q = quality.for_table([[a, b, a, c], [a, d, c, e]], 3)
+        q = quality_for_table([[a, b, a, c], [a, d, c, e]], 3)
         self.assertEqual(1, q.excess_ss)
         self.assertAlmostEqual(3, q.image_shrinkage, places=2)
         self.assertEqual(12, q.bad_breaks)
@@ -150,7 +169,7 @@ class TestQualityComparisonForGroups(unittest.TestCase):
         self.assertEqual(3, q.unplaced)
         self.assertEqual(None, q.height_dev)
 
-        q = quality.for_columns([50, 70], [[a, b, a, c], [a, d, c, e]], 3)
+        q = quality_for_columns([50, 70], [[a, b, a, c], [a, d, c, e]], 3)
         self.assertEqual(1, q.excess_ss)
         self.assertAlmostEqual(3, q.image_shrinkage, places=2)
         self.assertEqual(12, q.bad_breaks)
@@ -165,10 +184,10 @@ class TestQualityComparisonForGroups(unittest.TestCase):
         d = quality.for_image('normal', IM_DESIRED, Rect(0, 50, 0, 100), Rect(0, 100, 0, 200))
         e = quality.for_image('normal', IM_DESIRED, Rect(0, 100, 0, 100), Rect(0, 100, 0, 200))
 
-        t1 = quality.for_table([[a, b], [a, c]], 3)
-        t2 = quality.for_table([[d, e], [b, b]], 7)
+        t1 = quality_for_table([[a, b], [a, c]], 3)
+        t2 = quality_for_table([[d, e], [b, b]], 7)
 
-        q = quality.for_columns([50, 70], [[t1, t1], [t1, t2]], 3)
+        q = quality_for_columns([50, 70], [[t1, t1], [t1, t2]], 3)
         self.assertEqual(4, q.excess_ss)
         self.assertAlmostEqual(4.0, q.image_shrinkage, places=2)
         self.assertEqual(6 * 7, q.bad_breaks)
@@ -188,14 +207,14 @@ class TestQualityComparisonForGroups(unittest.TestCase):
         half_image = quality.for_image('normal', IM_DESIRED, Rect(0, 50, 0, 200), Rect(0, 50, 0, 200))
 
         # Just checking that perfect really is
-        self.assertEqual(0, quality.for_table([[perfect_image], [perfect_text]], 3).minor_score())
+        self.assertEqual(0, quality_for_table([[perfect_image], [perfect_text]], 3).minor_score())
 
-        a = quality.for_table([[perfect_image], [no_break_10_excess]], 3)
-        b = quality.for_table([[slightly_shrunk_image], [perfect_text]], 3)
-        c = quality.for_table([[slightly_shrunk_image], [no_break_20_excess]], 3)
-        d = quality.for_table([[perfect_image], [bad_break_no_excess]], 3)
-        e = quality.for_table([[half_image], [perfect_text]], 3)
-        f = quality.for_table([[bad_break_no_excess], [bad_break_no_excess]], 3)
+        a = quality_for_table([[perfect_image], [no_break_10_excess]], 3)
+        b = quality_for_table([[slightly_shrunk_image], [perfect_text]], 3)
+        c = quality_for_table([[slightly_shrunk_image], [no_break_20_excess]], 3)
+        d = quality_for_table([[perfect_image], [bad_break_no_excess]], 3)
+        e = quality_for_table([[half_image], [perfect_text]], 3)
+        f = quality_for_table([[bad_break_no_excess], [bad_break_no_excess]], 3)
 
         self.assertTrue(a.better(b))
         self.assertTrue(b.better(c))
@@ -208,9 +227,9 @@ class TestQualityComparisonForGroups(unittest.TestCase):
         none = quality.for_wrapping(10, 0, 0)
         one = quality.for_wrapping(10, 0, 1)
 
-        no_breaks_5_height_difference = quality.for_columns([100, 105], [[none], [none, none]], 0)
-        no_breaks_40_height_difference = quality.for_columns([140, 100], [[none], [none, none]], 0)
-        one_break_0_height_difference = quality.for_columns([105, 105], [[none], [none, one]], 0)
+        no_breaks_5_height_difference = quality_for_columns([100, 105], [[none], [none, none]], 0)
+        no_breaks_40_height_difference = quality_for_columns([140, 100], [[none], [none, none]], 0)
+        one_break_0_height_difference = quality_for_columns([105, 105], [[none], [none, one]], 0)
 
         self.assertTrue(no_breaks_5_height_difference.better(one_break_0_height_difference))
         self.assertTrue(one_break_0_height_difference.better(no_breaks_40_height_difference))
